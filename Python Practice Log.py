1542ea66486c3814d29c2231a074f3c3f88f9b9f
kellychr <kellychr@urbandaleschools.com>
2022-09-09 08:59:31 -0500

    python Practice

---
+++ Python Practice Log.py
@@ -0,0 +1,35 @@
+'''this is my learning about python
+this is another line
+'''
+
+print(10.1//2) #floor division
+print(7%2) #modulus get the remainder
+print(abs(-7)) #absolute value
+a=2
+b=7
+print (a*b)
+print(b**a)
+print(b%2)
+
+a,b=(5,"christan")
+print(a)
+print(b)
+
+x=8
+print(x)
+x-=2 #same as x=x*2
+print(x)
+
+bullets_Shot=0
+bulletsShot=3
+print(bulletsShot)
+a="this is some text"
+
+b=3
+c=4.3
+d=True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
